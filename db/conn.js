cd3c38edb4b2fedb03c00b9e2f651456ae8c97fe
DylanC <comeaudc@comcast.net>
2026-02-03 13:28:25 -0500

    Added Schema Function

--- db/conn.js
+++ db/conn.js
@@ -17,8 +17,8 @@ try {
 
   db = conn.db("sample_training");
 
-  await createCollectionSetup()
-
+  await createCollectionSetup();
+  await createGradesSchema();
 } catch (error) {
   console.error(error);
   process.exit(1);
@@ -28,7 +28,44 @@ async function createCollectionSetup() {
   const grades = db.collection("grades");
 
   await grades.createIndex({ learner_id: -1 });
-  await grades.createIndex({class_id: 1})
+  await grades.createIndex({ class_id: 1 });
+  // await grades.class_id({"scores.score": 1});
+}
+
+async function createGradesSchema() {
+  await db.command({
+    collMod: "grades",
+    validator: {
+      $jsonSchema: {
+        bsonType: "object",
+        required: ["learner_id", "class_id", "scores"],
+        properties: {
+          learner_id: {
+            bsonType: "int",
+          },
+          class_id: {
+            bsonType: "int",
+          },
+          scores: {
+            bsonType: "array",
+            items: {
+              bsonType: "object",
+              required: ["type", "score"],
+              properties: {
+                type: {
+                  enum: ["quiz", "exam", "homework"],
+                },
+                score: {
+                  bsonType: ["int", "double"],
+                  minimum: 0,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
 }
 
 export default db;
